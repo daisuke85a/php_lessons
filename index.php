fde4a944c2f2b7b2e4d000a0f1a86b9bea635651
vagrant <vagrant@localhost.localdomain>
2018-06-30 10:16:38 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "hello from the TOP!";
+echo "hello from the TOP! again";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+    <p>Hello world <?php echo" from PHP"; ?></p>
+</body>
+</html>
