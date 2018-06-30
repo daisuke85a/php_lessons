ffb6916f8e992f19c974e22562016e4a9d31b8eb
Daisuke Saito <dsaito85a@gmail.com>
2018-06-30 10:22:01 +0900

    study ready

---
+++ 28/index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<heml lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Check username</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="username" placeholder="user name">
+        <input type="submit" value="Check!">
+    </form>
+</body>
+</html>
