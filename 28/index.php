9fb0ddebf6c61a935a6088c69e2afee5075163a1
Daisuke Saito <dsaito85a@gmail.com>
2018-06-30 10:36:18 +0900

    mycode but not post action

--- 28/index.php
+++ 28/index.php
@@ -1,17 +1,27 @@
 <?php
-
+$username = '';
+if($_SERVER['REAUEST_METHOD'] === 'POST' ) {
+    echo "post";
+    $username = $_POST['username'];
+    $err = false;
+    if (strlen($username) > 8){
+        $err = true;
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<heml lang="ja">
+<html lang="ja">
 <head>
     <meta charset="utf-8">
     <title>Check username</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <input type="text" name="username" placeholder="user name">
+    <form action="index.php" method="POST">
+        <input type="text" name="username" placeholder="user name" value="<?php echo 
+        htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?>">
         <input type="submit" value="Check!">
+        <?php if ($err) { echo "Too long!"; } ?>
     </form>
 </body>
 </html>
